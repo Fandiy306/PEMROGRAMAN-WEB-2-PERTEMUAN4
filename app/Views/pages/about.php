80b7d902b509f1009cff0fd638c5dad53879fd5b
fandiy306 <fandiahmad@saintek.unipdu.ac.id>
2024-03-17 22:39:25 +0700

    Viws

---
+++ app/Views/pages/about.php
@@ -0,0 +1,15 @@
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h1>Hallo, Saya Fandi Ahmad</h1>
+            <p>Menikmati hidup adalah sebuah seni yang melibatkan kesadaran, penghargaan, dan kehadiran penuh dalam setiap momen. Berikut adalah sebuah kalimat yang menggambarkan konsep tersebut:
+
+"Dalam kompleksitas hidup ini, kita sebagai manusia sering kali terjebak dalam keramaian pikiran dan kecemasan masa depan. Namun, dengan menggunakan alat kebijaksanaan yang disebut kesadaran, kita dapat menyesuaikan diri dengan alur hidup yang berubah dengan cepat. Dalam setiap napas yang kita ambil, dalam setiap langkah yang kita ambil, kita menemukan keindahan dalam keberadaan kita yang sederhana. Seperti filter Kalman yang menyesuaikan perkiraan berdasarkan pengamatan aktual, kesadaran kita membantu kita untuk memfilter kekacauan dan menemukan kejelasan dalam setiap pengalaman yang kita hadapi. Dengan demikian, kita belajar untuk menyesuaikan diri dengan perubahan, merayakan kebahagiaan dalam momen-momen kecil, dan menemukan kedamaian dalam keadaan yang tidak pasti."</p>
+
+            <?php 
+            d($tes);
+            ?>
+
+        </div>
+    </div>
+</div>
